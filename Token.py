5b2ac0d15d7ab06c8f3c4d55be8ad6518665cc51
Gopika Jayanthakumar Latha <153133768+GopikaJL@users.noreply.github.com>
2025-01-02 19:09:40 +0100

    Update Token.py

--- Token.py
+++ Token.py
@@ -1,1 +1,13 @@
-
+from kaggle_secrets import UserSecretsClient
+from IPython.display import display
+from IPython.display import Markdown
+import os
+import pathlib
+import textwrap
+user_secrets = UserSecretsClient()
+apiKey = user_secrets.get_secret("GEMINI_API_KEY")
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+import google.generativeai as genai
+genai.configure(api_key = apiKey)
